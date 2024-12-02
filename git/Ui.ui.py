5d7d4f83494d48b0f5685a436807964842aa1ffe
katysidorova <Katy.sidorova.2008@yandex.ru>
2024-12-02 18:53:36 +0400

    final commit

--- git/Ui.ui.py
+++ git/Ui.ui.py
@@ -1,18 +1,19 @@
 import sys
 import random
 
+from PyQt6 import uic
 from PyQt6.QtGui import QPainter, QColor
 from PyQt6.QtWidgets import QWidget, QApplication, QPushButton
 from PyQt6.QtCore import QRect, QPoint
 
 
-class Example(QWidget):
+class Circles(QWidget):
     def __init__(self):
         super().__init__()
         self.initUI()
 
     def initUI(self):
-        self.setGeometry(300, 300, 200, 200)
+        self.setGeometry(0, 0, 2000, 2000)
         self.setWindowTitle('Рисование')
         self.btn = QPushButton('Рисовать', self)
         self.btn.move(70, 150)
@@ -32,7 +33,17 @@ class Example(QWidget):
         self.update()
 
     def draw_circle(self, qp):
-        qp.setBrush(QColor(255, 255, 0))
-        r = random.randint(0, 300)
+        r = random.randint(0, 2000)
+        a = random.randint(0, 255)
+        b = random.randint(0, 255)
+        c = random.randint(0, 255)
+        qp.setBrush(QColor(a, b, c))
         # rect = QRect(QPoint(0, 0), QPoint(r, r))
         qp.drawEllipse(0, 0, r, r)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = Circles()
+    ex.show()
+    sys.exit(app.exec())
